2f17496065fab32c31cd3ad0af0bd39109f6fa10
Derek Redfern <redfern.derek@gmail.com>
2014-08-04 21:52:59 -0700

    Simple main loop to exchange data with a remote device

--- dolores.ino
+++ dolores.ino
@@ -15,22 +15,66 @@
 //  if we are very careful and make the appropriate sacrifices to the C gods.
 
                                 // BLE#   RX   TX   VIN   BLE ROLE      CONNECTED TO
-#define ble0 Serial             // 0      0    1    4     PERIPHERAL    PHONE
-SoftwareSerial ble1(10, 11);    // 1      10   11   5     PERIPHERAL    OTHER DEVICE
+#define ble0 Serial             // 0      0    1    5V    PERIPHERAL    PHONE
+SoftwareSerial ble1(10, 11);    // 1      10   11   4     PERIPHERAL    OTHER DEVICE
 
-#define btnPin -1 // what pin is this on again?
-#define ledPin 13
+#define btnPin        12                // user input from physical button
+#define ledPin        13                // LED debug pin
+#define idNum         "TEST ID NUMBER"  // ID number to send to a remote phone
+#define ble1_vin_pin  4                 // used to turn the 2nd BLE chip on/off
+
+int buttonVal; // set whenever the debounced button value changes
+int lastButtonVal; // remembers what the undebounced button value was last loop
+unsigned long debounce; // remembers when the lastButtonVal changed last
+int bleAdvertising;
 
 void setup(void)
-{ 
-  // We can support higher speeds than 57600, but this is what's preprogrammed
-  //  on the TI BLE chip.
+{
   ble0.begin(57600);
   ble1.begin(57600);
+
+  pinMode(btnPin, INPUT);
+  pinMode(ledPin, OUTPUT); 
+  pinMode(ble1_vin_pin, OUTPUT);
+
+  digitalWrite(ledPin,LOW);
+  digitalWrite(ble1_vin_pin,LOW);
+
+  buttonVal = LOW;
+  lastButtonVal = LOW;
+  debounce = -1;
+  bleAdvertising = false;
 }
 
 void loop()
 {
-  // For now, there's nothing here. 
-  // Oh well.
+  /* BUTTON DEBOUNCING */
+  int buttonRead = digitalRead(btnPin);
+
+  if (lastButtonVal != buttonRead) {
+    debounce = millis();
+  }
+
+  if (millis() - debounce > 100 && buttonRead != buttonVal) {
+    buttonVal = buttonRead;
+
+    if (buttonVal==HIGH) {
+      // let the phone know that the button was pressed so it can look for devices
+      ble0.write('A');
+
+      // activate the second BLE chip
+      digitalWrite(ble1_vin_pin,HIGH);
+      bleAdvertising = true;
+    }
+  }
+
+  /* HANDLING REQUESTS FROM REMOTE PHONE */
+  if (bleAdvertising) {
+    if (ble1.available()) {
+      if (ble1.read()=='B') {
+        // another phone wants profile data; send it our ID number for lookup in the cloud
+        ble1.println(idNum);
+      }
+    }
+  }
 }

85f9e146b582a0e3a9805806d7d4a12f8c153ccb
Derek Redfern <redfern.derek@gmail.com>
2014-09-01 16:51:53 -0400

    Changed SoftwareSerial to AltSoftSerial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 dolores
 =======
+
+Prerequisites:
+* AltSoftSerial installed. Version 1.2 is included in this repo; see http://arduino.cc/en/Guide/Libraries for help installing libraries on Arduino.
--- dolores.ino
+++ dolores.ino
@@ -7,19 +7,20 @@
  */
 
 #include <SoftwareSerial.h>
+#include <AltSoftSerial.h>
 
 // BLE0 will be on the hardware serial port, while BLE1 will be on a 
 //  software serial port.
 
                                 // BLE#   RX   TX   VIN   BLE ROLE      CONNECTED TO
 #define ble0 Serial1            // 0      0    1    5V    PERIPHERAL    PHONE
-SoftwareSerial ble1(10, 11);    // 1      10   11   4     PERIPHERAL    OTHER DEVICE
+AltSoftSerial ble1;             // 1      13   5    4     PERIPHERAL    OTHER DEVICE
 
 #define btnPin        12                // user input from physical button
-#define ledPin        13                // LED debug pin
+#define ledPin        14                // LED debug pin
 #define idNum         "TEST ID NUMBER"  // ID number to send to a remote phone
 #define ble1_vin_pin  4                 // used to turn the 2nd BLE chip on/off
-#define bleTimeout    10000     // milliseconds to advertise for
+#define bleTimeout    10000             // milliseconds to advertise for
 
 int buttonVal; // set whenever the debounced button value changes
 int lastButtonVal; // remembers what the undebounced button value was last loop
@@ -77,7 +78,8 @@ void loop()
       char charRead = ble1.read(); // Seems to read garbled characters using SoftwareSerial
       if (charRead=='B') {
         // another phone wants profile data; send it our ID number for lookup in the cloud
-        ble1.println(idNum);
+        ble1.print(idNum);
+        ble1.print('\n');
       }
     }
   }
